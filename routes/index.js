ea896b2b9798bdf58b481948bcec4178c62f4d7a
lideo <1573567+lideo@users.noreply.github.com>
2019-09-11 19:02:30 +0800

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Send10' });
+});
+
+router.post('/charge', function(req, res, next) {
+  res.send('charge!!!');
+});
+
+module.exports = router;
